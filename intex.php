1a67c487833b6219f4689860deba182e7b536886
JakubVavru <60426585+JakubVavru@users.noreply.github.com>
2020-04-14 10:06:48 +0200

    Lísky
    jej

---
+++ intex.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$age = 12;
+$height = 140;
+/*
+        if (($age > 15) || ($age > 65)) {
+            $entryfee = 100;
+        } else {
+            $entryfee = 50;
+        }
+*/
+        if (($age <= 10) && ($height <= 140) || ($age >= 65)) {
+            $entryfee = 60;
+        } elseif ( ($age >= 15) && ($age <= 26) ) {
+            $entryfee = 80;
+        } else {
+            $entryfee = 120;
+        }
+          
+
+?>
+
+<h1> Je ti <?= $age?> let a měříš <?= $height?>cm, tak platíš <?= $entryfee?> Kč. </h1>
+
+</body>
+</html>
